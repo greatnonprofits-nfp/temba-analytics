1cd2c1c9a73e9877cc972514c1ef845f79f71db1
Ihor Palii <ithor@communityconnectlabs.com>
2021-07-29 22:47:42 +0300

    Completed report select component.

--- src/components/reports/Reports.tsx
+++ src/components/reports/Reports.tsx
@@ -1,34 +1,62 @@
 import React from "react";
 import "./Reports.scss"
+import {renderIf} from "../../utils";
+import {Report} from "../../utils/types";
 
 interface ReportsProps {
-  reports: any[]
+  reports: Report[],
+  onReportSelected: (report: Report) => any,
 }
 
 interface ReportsState {
+  reports: Report[],
+  currentReport: Report | null,
 }
 
 
 export default class Reports extends React.Component<ReportsProps, ReportsState> {
   constructor(props: ReportsProps) {
     super(props);
-    this.state = {};
+    this.state = {reports: this.props.reports, currentReport: null};
+  }
+
+  private isReportActive(report: Report) {
+    let currentReport = this.state.currentReport
+    if (currentReport !== null) {
+      return currentReport.id === report.id;
+    }
+    return false;
+  }
+
+  private setReportActive(report: Report) {
+    this.setState({currentReport: report});
+    this.props.onReportSelected(report);
   }
 
   render() {
     return <div className="reports-container">
-      <div className="title">Reports</div>
-      <div className="inner-scroll">
-        {this.props.reports.map((report: any) => (
-          <div className="report-item">
-            <div className="nav-item">
-              <div className="name">
-                {report.name}
+      {renderIf(this.state.reports.length > 0)(
+        <>
+          <div className="title">Reports</div>
+          <div className="inner-scroll">
+            {this.state.reports.map((report: Report, idx) => (
+              <div
+                key={idx}
+                className={"report-item" + (this.isReportActive(report) ? " active" : "")}
+                onClick={() => {
+                  this.setReportActive(report);
+                }}
+              >
+                <div className="nav-item">
+                  <div className="name">
+                    {report.text}
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
     </div>;
   }
 }
