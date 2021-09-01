66b3c8f8260728b1b21d26eeacf696985fba55ce
Ihor Palii <ithor@communityconnectlabs.com>
2021-09-01 16:39:24 +0300

    Indeterminate checkbox param.

--- src/components/checkbox/CheckBox.tsx
+++ src/components/checkbox/CheckBox.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import "./CheckBox.scss";
 
 interface CheckBoxProps {
-  checked: boolean,
-  onChecked: (checked: boolean) => any,
+  checked: boolean | null,
+  onChecked: (checked: boolean | null) => any,
+  indeterminate?: boolean,
   label: string,
 }
+
 interface CheckBoxState {
-  checked: boolean,
+  checked: boolean | null,
   label: string,
 }
 
 
-export default class CheckBox extends React.Component<CheckBoxProps, CheckBoxState>{
+export default class CheckBox extends React.Component<CheckBoxProps, CheckBoxState> {
   constructor(props: CheckBoxProps) {
     super(props);
     this.state = {checked: this.props.checked, label: this.props.label};
@@ -23,14 +25,37 @@ export default class CheckBox extends React.Component<CheckBoxProps, CheckBoxSta
   }
 
   private handleOnChecked() {
-    let checked = !this.state.checked;
+    let checked: boolean | null = true;
+    if (this.props.indeterminate) {
+      if (this.state.checked === true) {
+        checked = false;
+      } else if (this.state.checked === false) {
+        checked = null;
+      }
+    } else {
+      checked = !this.state.checked;
+    }
     this.setState({checked})
     this.props.onChecked(checked);
   }
 
+  private getCurrentStateIcon() {
+    if (this.props.indeterminate) {
+      if (this.state.checked === true) {
+        return "fa-check-square";
+      } else if (this.state.checked === false) {
+        return "fa-minus-square"
+      } else {
+        return "fa-square"
+      }
+    } else {
+      return this.state.checked ? "fa-check-square" : "fa-square";
+    }
+  }
+
   render() {
     return <div className={"checkbox-container"} onClick={this.handleOnChecked.bind(this)}>
-      <i className={"far" + (this.state.checked ? " fa-check-square" : " fa-square")}/>
+      <i className={"far " + this.getCurrentStateIcon()}/>
       <div className="checkbox-label">{this.state.label}</div>
     </div>;
   }
